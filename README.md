4e99a95359d090e3bfad21a84daf1a6a8251910a
(hyebin) <(sksp4334@naver.com)>
2021-07-20 23:39:35 +0900

    readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## react-portfolio
 
-1. intersection observer API 추가
+1. app.js 초기화
+2. GSAP scolltriger정리
+3. 포트폴리오 설계
 
 ### 다음할 것
--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-import Greet from "./component/greet/greet";
 import styled, { createGlobalStyle } from "styled-components";
-import Nav from "./component/nav/nav";
-import { useRef } from "react";
-import { useIntersection } from "react-use";
-import gsap from "gsap";
 
 const GlobalStyle = createGlobalStyle`
   *{
@@ -16,6 +11,9 @@ const GlobalStyle = createGlobalStyle`
   body{
     font-family: 'Otomanopee One', sans-serif ;
     scroll-behavior: smooth;
+    scrollbar-color: black;
+    scrollbar-width: thin;
+    scrollbar-track-color: #fff;
   }
   a{
     text-decoration:none;
@@ -27,99 +25,41 @@ const GlobalStyle = createGlobalStyle`
     color:white;
     font-size:1.5rem;
 `;
+
+const Greeting = styled.div``;
+const ImgBox = styled.div``;
+const H2 = styled.h2``;
+
 const Wrapper = styled.section`
-  scroll-snap-type: y mandatory;
-  height: 100vh;
-  overflow-y: scroll;
-`;
-const Section = styled.section`
-  position: relative;
-  height: 100vh;
-  max-width: 1200px;
-  margin: 0 auto;
-  border-left: 1px solid #e5e5e5;
-  border-right: 1px solid #e5e5e5;
-  width: 100%;
-  scroll-snap-align: start;
-  scroll-snap-stop: normal;
-  background: ${(props) => (props.color ? props.color : " white")};
-  transition: 0.3s;
-`;
-const H2 = styled.h2`
-  text-align: center;
-  color: blue;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    width: 5px; /*스크롤바의 너비*/
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: black; /*스크롤바의 색상*/
+  }
 `;
 function App() {
-  const section1Ref = useRef(null);
-  const section2Ref = useRef(null);
-  const section3Ref = useRef(null);
-
-  const option = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 1,
-  };
-
-  const section1Intersection = useIntersection(section1Ref, option);
-  const section2Intersection = useIntersection(section2Ref, option);
-  const section3Intersection = useIntersection(section3Ref, option);
-
-  const fadeIn = (element) => {
-    gsap.to(element, 1, {
-      opacity: 1,
-      y: -60,
-      ease: "power4.out",
-      stagger: {
-        amount: 0.3,
-      },
-    });
-  };
-  const fadeOut = (element) => {
-    gsap.to(element, 1, {
-      opacity: 0,
-      y: -20,
-      ease: "power4.out",
-    });
-  };
-
-  section1Intersection && section1Intersection.isIntersecting
-    ? fadeIn(".text1")
-    : fadeOut(".text1");
-
-  section2Intersection && section2Intersection.isIntersecting
-    ? fadeIn(".text2")
-    : fadeOut(".text2");
-  section2Intersection && section2Intersection.isIntersecting
-    ? (section2Ref.current.style = "background :#222831")
-    : fadeOut(".text2");
-  section3Intersection && section3Intersection.isIntersecting
-    ? fadeIn(".text3")
-    : fadeOut(".text3");
-  section3Intersection && section3Intersection.isIntersecting
-    ? (section3Ref.current.style = "background :#222831")
-    : fadeOut(".text3");
   return (
     <Wrapper>
-      <Section ref={section1Ref}>
-        <GlobalStyle />
-        <Nav />
-        <Greet />
-      </Section>
-      <Section ref={section1Ref}>
-        <H2 className="text1">
-          안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
-        </H2>
-      </Section>
-      <Section ref={section2Ref}>
-        <H2 className="text2">
-          안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
-        </H2>
-      </Section>
-      <Section ref={section3Ref}>
-        <H2 className="text3">
-          안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
-        </H2>
-      </Section>
+      <GlobalStyle />
+      <Greeting />
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <H2>h2</H2>
+      <ImgBox />
     </Wrapper>
   );
 }
